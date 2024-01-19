44e7dd8d68c8f01f5ab444dd26d7aafe4da0661b
SCaucota <masocaucota@gmail.com>
2024-01-19 20:17:11 -0300

    Tests Unitarios y de Integración

--- src/models/libroModel.js
+++ src/models/libroModel.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
-
-mongoose.connect(process.env.MONGO_DB, {
-  useUnifiedTopology: true,
+mongoose.connect('mongodb://localhost:27017/biblioteca', {
   useNewUrlParser: true,
+  useUnifiedTopology: true
 });
 
 const LibroSchema = new mongoose.Schema({
